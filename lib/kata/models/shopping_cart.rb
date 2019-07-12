07b81bab52acfebbaf64d6d8331e06545503e56b
Kyle <kjdchapman@gmail.com>
2019-07-12 15:21:33 +0100

    TCR!

--- lib/kata/models/shopping_cart.rb
+++ lib/kata/models/shopping_cart.rb
@@ -44,26 +44,21 @@ class Kata::ShoppingCart
       unit_price = catalog.unit_price(product)
       quantity_as_int = quantity.to_i
       discount = nil
-      x = nil
       number_of_x = nil
 
       if offer.offer_type == Kata::SpecialOfferType::THREE_FOR_TWO
-        x = 3
-        number_of_x = quantity_as_int / x
+        number_of_x = quantity_as_int / 3
       elsif offer.offer_type == Kata::SpecialOfferType::TWO_FOR_AMOUNT
-        x = 2
-        number_of_x = quantity_as_int / x
+        number_of_x = quantity_as_int / 2
         if quantity_as_int >= 2
-          total = offer.dangerously_overloaded_argument_for_modifying_offers_of_different_types * quantity_as_int / x + quantity_as_int % 2 * unit_price
+          total = offer.dangerously_overloaded_argument_for_modifying_offers_of_different_types * quantity_as_int / 2 + quantity_as_int % 2 * unit_price
           discount_n = unit_price * quantity - total
           discount = Kata::Discount.new(product, "2 for " + offer.dangerously_overloaded_argument_for_modifying_offers_of_different_types.to_s, discount_n)
         end
       elsif offer.offer_type == Kata::SpecialOfferType:: FIVE_FOR_AMOUNT
-        x = 5
-        number_of_x = quantity_as_int / x
+        number_of_x = quantity_as_int / 5
       else
-        x = 1
-        number_of_x = quantity_as_int / x
+        number_of_x = quantity_as_int
       end
 
 
