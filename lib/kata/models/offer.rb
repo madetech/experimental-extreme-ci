21c72687c34291a9e6c23b52c9fa497f6407ab98
Craig J. Bass <craig@madetech.co.uk>
2019-07-12 15:48:20 +0100

    TCR!

--- lib/kata/models/offer.rb
+++ lib/kata/models/offer.rb
@@ -8,4 +8,29 @@ class Kata::Offer
     @product = product
   end
 
+  def apply(catalog, product, quantity)
+    unit_price = catalog.unit_price(product)
+    quantity_as_int = quantity.to_i
+    discount = nil
+
+    if offer_type == Kata::SpecialOfferType::THREE_FOR_TWO && quantity_as_int >= 3
+      discount_amount = quantity * unit_price - (((quantity_as_int / 3) * 2 * unit_price) + quantity_as_int % 3 * unit_price)
+      discount = Kata::Discount.new(product, "3 for 2", discount_amount)
+    elsif offer_type == Kata::SpecialOfferType::TWO_FOR_AMOUNT
+      if quantity_as_int >= 2
+        total = dangerously_overloaded_argument_for_modifying_offers_of_different_types * quantity_as_int / 2 + quantity_as_int % 2 * unit_price
+        discount_n = unit_price * quantity - total
+        discount = Kata::Discount.new(product, "2 for " + dangerously_overloaded_argument_for_modifying_offers_of_different_types.to_s, discount_n)
+      end
+    elsif offer_type == Kata::SpecialOfferType::FIVE_FOR_AMOUNT
+      if quantity_as_int >= 5
+        discount_total = unit_price * quantity - (dangerously_overloaded_argument_for_modifying_offers_of_different_types * (quantity_as_int / 5) + quantity_as_int % 5 * unit_price)
+        discount = Kata::Discount.new(product, "5 for " + dangerously_overloaded_argument_for_modifying_offers_of_different_types.to_s, discount_total)
+      end
+    elsif offer_type == Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT
+      discount = Kata::Discount.new(product, dangerously_overloaded_argument_for_modifying_offers_of_different_types.to_s + "% off", quantity * unit_price * dangerously_overloaded_argument_for_modifying_offers_of_different_types / 100.0)
+    end
+
+    discount
+  end
 end
--- lib/kata/models/shopping_cart.rb
+++ lib/kata/models/shopping_cart.rb
@@ -44,27 +44,8 @@ class Kata::ShoppingCart
       quantity = @cart_items.quantity_of(product)
 
       offer = offers.for(product)
-      unit_price = catalog.unit_price(product)
-      quantity_as_int = quantity.to_i
-      discount = nil
 
-      if offer.offer_type == Kata::SpecialOfferType::THREE_FOR_TWO && quantity_as_int >= 3
-        discount_amount = quantity * unit_price - (((quantity_as_int / 3) * 2 * unit_price) + quantity_as_int % 3 * unit_price)
-        discount = Kata::Discount.new(product, "3 for 2", discount_amount)
-      elsif offer.offer_type == Kata::SpecialOfferType::TWO_FOR_AMOUNT
-        if quantity_as_int >= 2
-          total = offer.dangerously_overloaded_argument_for_modifying_offers_of_different_types * quantity_as_int / 2 + quantity_as_int % 2 * unit_price
-          discount_n = unit_price * quantity - total
-          discount = Kata::Discount.new(product, "2 for " + offer.dangerously_overloaded_argument_for_modifying_offers_of_different_types.to_s, discount_n)
-        end
-      elsif offer.offer_type == Kata::SpecialOfferType::FIVE_FOR_AMOUNT
-        if quantity_as_int >= 5
-          discount_total = unit_price * quantity - (offer.dangerously_overloaded_argument_for_modifying_offers_of_different_types * (quantity_as_int / 5) + quantity_as_int % 5 * unit_price)
-          discount = Kata::Discount.new(product, "5 for " + offer.dangerously_overloaded_argument_for_modifying_offers_of_different_types.to_s, discount_total)
-        end
-      elsif offer.offer_type == Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT
-        discount = Kata::Discount.new(product, offer.dangerously_overloaded_argument_for_modifying_offers_of_different_types.to_s + "% off", quantity * unit_price * offer.dangerously_overloaded_argument_for_modifying_offers_of_different_types / 100.0)
-      end
+      discount = offer.apply(catalog, product, quantity)
 
       receipt.add_discount(discount) if discount
     end
