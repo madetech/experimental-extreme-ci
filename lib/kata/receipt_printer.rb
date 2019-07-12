841bef67233989ea7c499236f20c5e182f90f3f0
Kyle <kjdchapman@gmail.com>
2019-07-12 16:16:38 +0100

    TCR!

--- lib/kata/receipt_printer.rb
+++ lib/kata/receipt_printer.rb
@@ -11,11 +11,7 @@ class Kata::ReceiptPrinter
     end
 
     for discount in receipt.discounts do
-      product_presentation = discount.product.product_name
-      price_presentation = "%.2f" % discount.discount_amount
-      description = discount.description
-      whitespace = self.class.whitespace(@columns - 3 - product_presentation.size - description.size - price_presentation.size)
-      result += "#{description}(#{product_presentation})#{whitespace}-#{price_presentation}\n"
+      result += print_discount(discount)
     end
     result.concat("\n")
     price_presentation = "%.2f" % receipt.total_price.to_f
@@ -23,6 +19,14 @@ class Kata::ReceiptPrinter
     whitespace = self.class.whitespace(@columns - total.size - price_presentation.size)
     result.concat(total, whitespace, price_presentation)
     return result.to_s
+  end
+
+  def print_discount(discount)
+    product_presentation = discount.product.product_name
+    price_presentation = "%.2f" % discount.discount_amount
+    description = discount.description
+    whitespace = self.class.whitespace(@columns - 3 - product_presentation.size - description.size - price_presentation.size)
+    "#{description}(#{product_presentation})#{whitespace}-#{price_presentation}\n"
   end
 
   def print_item(item)
