28df41c7c14a7860eaf84ff96de0faba0027c14b
Kyle <kjdchapman@gmail.com>
2019-07-12 14:37:07 +0100

    TCR!

--- lib/kata/models/offer.rb
+++ lib/kata/models/offer.rb
@@ -2,9 +2,9 @@ class Kata::Offer
 
   attr_reader :product, :offer_type, :argument
 
-  def initialize(offer_type, product, argument)
+  def initialize(offer_type, product, dangerously_overloaded_argument_for_modifying_offers_of_different_types)
     @offer_type = offer_type
-    @argument = argument
+    @argument = dangerously_overloaded_argument_for_modifying_offers_of_different_types
     @product = product
   end
 
