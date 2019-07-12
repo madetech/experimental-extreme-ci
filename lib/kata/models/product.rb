508b6f23fd25ee710782701ddb8c059a85b05a28
Kyle <kjdchapman@gmail.com>
2019-07-12 14:45:46 +0100

    TCR!

--- lib/kata/models/product.rb
+++ lib/kata/models/product.rb
@@ -1,8 +1,8 @@
 class Kata::Product
   attr_reader :product_name, :unit
 
-  def initialize(product_name, unit)
+  def initialize(product_name, unit_of_measure)
     @product_name = product_name
-    @unit = unit
+    @unit = unit_of_measure
   end
 end
