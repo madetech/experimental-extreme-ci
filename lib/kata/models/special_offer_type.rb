f01ce2a8a37096796fa2f5df43f7fee41551b094
Craig J. Bass <craig@madetech.co.uk>
2019-07-12 15:23:03 +0100

    TCR!

--- lib/kata/models/special_offer_type.rb
+++ lib/kata/models/special_offer_type.rb
@@ -1,5 +1,5 @@
 module Kata::SpecialOfferType
-  THREE_FOR_TWO = Object.new
+  THREE_FOR_TWO = "ThreeForTwo".freeze
   TEN_PERCENT_DISCOUNT = Object.new
   TWO_FOR_AMOUNT = Object.new
   FIVE_FOR_AMOUNT = Object.new
